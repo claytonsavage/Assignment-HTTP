629d8638545f8bc163de7e3fb593f32c9887a905
claytonsavage <clytnsavage@gmail.com>
2017-12-04 21:24:17 +0000

    finished step 5

---
+++ http-functions.js
@@ -0,0 +1,31 @@
+module.exports = function getHTML (options, callback) {
+  var https = require('https');
+  var chunksArray = [];
+  var html = [];
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (chunk) {
+      chunksArray.push(chunk);
+    });
+
+    response.on('end', function() {
+      html = chunksArray.join();
+      return callback(html);
+    });
+
+  });
+}
+
+var printHTML = function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step2.html'
+};
+
+
